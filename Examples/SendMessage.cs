1eb30d7bac0a04baab3b53c26784764901a72f2d
A-mont <a.mont.24.777@gmail.com>
2024-07-24 15:31:48 -0600

    Add SendMessage

--- Examples/SendMessage.cs
+++ Examples/SendMessage.cs
@@ -3,6 +3,7 @@ using System;
 using Schnorrkel.Keys;
 using System.Threading;
 using Substrate.NetApi;
+using System.Text;
 using Substrate.NetApi.Model.Extrinsics;
 using Substrate.NetApi.Model.Rpc;
 using Substrate.NetApi.Model.Types;
@@ -10,53 +11,23 @@ using Substrate.NetApi.Model.Types.Base;
 using Substrate.NetApi.Model.Types.Primitive;
 using Substrate.Vara.NET.NetApiExt.Generated.Storage;
 using VaraExt = Substrate.Vara.NET.NetApiExt.Generated;
+using Substrate.Vara.NET.NetApiExt.Generated.Model.sp_core.crypto;
+using Substrate.Vara.NET.NetApiExt.Generated.Model.gear_core.ids;
 
 
 
 
-public class SendMessage : MonoBehaviour
+public class SendMessageExtrinsic : MonoBehaviour
 {
-    private VaraExt.SubstrateClientExt _clientvara;
-    private string url;
+    private VaraExt.SubstrateClientExt _clientvara; // Substrate client to interact with the Vara network
+    private string url; // Test network node URL
 
-    public static MiniSecret MiniSecretBob
-    {
-        get
-        {
-            var bytes = Utils.HexToByteArray("0x397aee0b14f2f82b2ff0b99d901c1e7a76dc80d65ac1a5f9c7e222b04cb1e973");
-            if (bytes == null || bytes.Length == 0)
-            {
-                throw new ArgumentException("HexToByteArray returned null or empty array");
-            }
-            return new MiniSecret(bytes, ExpandMode.Ed25519);
-        }
-    }
-
-    public static Account Bob
-    {
-        get
-        {
-            var secret = MiniSecretBob.ExpandToSecret().ToBytes();
-            var publicKey = MiniSecretBob.GetPair().Public.Key;
-
-            if (secret == null || secret.Length == 0)
-            {
-                throw new ArgumentException("ExpandToSecret returned null or empty array");
-            }
-            if (publicKey == null || publicKey.Length == 0)
-            {
-                throw new ArgumentException("GetPair().Public.Key returned null or empty array");
-            }
-
-            return Account.Build(KeyType.Sr25519, secret, publicKey);
-        }
-    }
-
+    // Property to get Alice's MiniSecret key
     public static MiniSecret MiniSecretAlice
     {
         get
         {
-            var bytes = Utils.HexToByteArray("0x496f9222372eca011351630ad276c7d44768a593cecea73685299e06acef8c0a");
+            var bytes = Utils.HexToByteArray("496f9222372eca011351630ad276c7d44768a593cecea73685299e06acef8c0a");
             if (bytes == null || bytes.Length == 0)
             {
                 throw new ArgumentException("HexToByteArray returned null or empty array");
@@ -65,6 +36,7 @@ public class SendMessage : MonoBehaviour
         }
     }
 
+    // Property to get Alice's account
     public static Account Alice
     {
         get
@@ -85,7 +57,6 @@ public class SendMessage : MonoBehaviour
         }
     }
 
-
     // Start is called before the first frame update
     async void Start()
     {
@@ -104,28 +75,62 @@ public class SendMessage : MonoBehaviour
             // Log a message indicating that the client is connected
             Debug.Log("Client is connected.");
 
-            // Creando el ProgramId (destination)
-           // var destination = new ProgramId();
-           // destination.Create("0x619701ff1f3e041069e70726679cef949cd86e826a20dcbe1bd9c8077c37c01d");
-            var payload = new BaseVec<U8>(new U8[0]);
-            var gas_limit = new U64(1000000);
-            var value = new U128(10000000000000);
+            // Define the amount to transfer
+            var amount = new BaseCom<U128>(4000000000000);
+
+            // Create the recipient account
+            var account32 = new AccountId32();
+            account32.Create("0xe4fa3b466792dcd7e58f5d8d49bc4631b5eec3a9ebe48ffe79f859dadf76cb71");
+            var multiAddress = new VaraExt.Model.sp_runtime.multiaddress.EnumMultiAddress();
+            multiAddress.Create(VaraExt.Model.sp_runtime.multiaddress.MultiAddress.Id, account32);
+
+            // Vara Account (Alice's account)
+            var aliceaccount32 = new AccountId32();
+            aliceaccount32.Create("0x96bc6c65c1a0579886003e9c796ac1a9a9e9c4abc7b74d3b1cf399aaf35d7139");
+
+            // Get the parameters needed to query account storage
+            string parameters = SystemStorage.AccountParams(aliceaccount32);
+
+            // Query the account info from the storage asynchronously
+            var accountInfo = await _clientvara.GetStorageAsync<VaraExt.Model.frame_system.AccountInfo>(parameters, null, CancellationToken.None);
+
+            // Log the free balance of the account
+            Debug.Log($"Balance: {accountInfo.Data.Free}");
+
+            // Prepare the sendMessage method call
+            var destination = new ProgramId();
+            destination.Create("0xd5d1eb08af166ac3bb210bac52814324a0e33501edb8c57e9102e81c820c09a2");
+            // Convert the string "Green" to a byte array
+            string message = "Green";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            // Convert the byte array to an array of U8
+            U8[] u8Array = new U8[messageBytes.Length];
+            for (int i = 0; i < messageBytes.Length; i++)
+            {
+                u8Array[i] = new U8(messageBytes[i]);
+            }
+
+            // Create the payload as a BaseVec<U8>
+            var payload = new BaseVec<U8>(u8Array);
+            var gas_limit = new U64(900000000000);
+            var value = new U128(0);
             var keep_alive = new Bool(true);
 
             // Llamando a GearCalls.SendMessage
-           // var sendMessage = GearCalls.SendMessage(destination, payload, gas_limit, value, keep_alive);
+            var sendMessage = GearCalls.SendMessage(destination, payload, gas_limit, value, keep_alive);
 
-            // Debug.Log($"Extrinsic submitted : {sendMessage}");
-            //Console.WriteLine($"Transaction : {sendMessage}");
+            Debug.Log($"Extrinsic submitted : {sendMessage}");
+            Console.WriteLine($"Transaction : {sendMessage}");
 
+            uint lifeTime = 64; // Set the lifetime for the extrinsic
 
-            // Enviar la transacción
-            uint lifetime = 64; // Lifetime in blocks
-            //Hash extrinsic = await _clientvara.Author.SubmitExtrinsicAsync(sendMessage, Alice, ChargeTransactionPayment.Default(), lifetime, CancellationToken.None);
+            // Send the extrinsic to the network and watch its status
+            var sendExtrinsic = await _clientvara.Author.SubmitAndWatchExtrinsicAsync(ExtrinsicStatusCallback, sendMessage, Alice, ChargeTransactionPayment.Default(), lifeTime);
+            //Hash response = await _clientvara.Author.SubmitExtrinsicAsync( transfer, Alice, ChargeTransactionPayment.Default(),lifeTime);
 
-
-            // Log the retrieved data to the debug console and the standard console
-            //Debug.Log($"Extrinsic: {extrinsic}");
+            Debug.Log($"extrinsic=> {sendExtrinsic}");
+            // Debug.Log($"response=> {response}");
         }
         else
         {
@@ -134,18 +139,14 @@ public class SendMessage : MonoBehaviour
         }
     }
 
-    
-  static byte[] HexToByteArray(string hex)
-  {
-    int NumberChars = hex.Length;
-    byte[] bytes = new byte[NumberChars / 2];
-    for (int i = 0; i < NumberChars; i += 2)
+    // Define the callback method for extrinsic status
+    private void ExtrinsicStatusCallback(string subscriptionId, ExtrinsicStatus status)
     {
-      bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        Debug.Log($"Subscription ID: {subscriptionId}");
+        Debug.Log($"Extrinsic Status: {status}");
     }
-    return bytes;
-  }
 
+    // Update is called once per frame
     void Update()
     {
         // You can add code here to be executed in each frame
